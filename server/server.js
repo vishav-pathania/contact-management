58196c26c3c0c4b6341f74fc0d33d799bd2df507
vishav-pathania <vishavpathania40@gmail.com>
2024-11-17 07:52:29 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors'); // Import CORS
 require('dotenv').config();
 
 const app = express();
@@ -8,11 +9,19 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(express.json());
+app.use(express.json()); // Parse incoming JSON
+
+// Configure CORS
+app.use(cors({ origin: 'http://localhost:3000' })); // Allow requests from the React app
 
 // Routes
 app.use('/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000;
+// Default route for testing
+app.get('/', (req, res) => {
+  res.send('Welcome to the Contact Management API');
+});
 
+// Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
